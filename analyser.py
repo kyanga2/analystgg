c3608f19ce84e0f6eb228b1b1bac8a90cda63a73
kyanga2 <kevinyang222@gmail.com>
2015-09-14 14:13:14 -0700

    only changes to docs on event handling

--- analyser.py
+++ analyser.py
@@ -9,7 +9,6 @@ varius statistics about the 10 players within the match
 
 from apiCaller import api_Caller
 
-
 '''
 Each match json contains the following fields we care about:
 
@@ -19,10 +18,46 @@ participants = game info about all 10 match_participants
 timeline = big timeline of events within the match
 participantIdentities = maps participantId to the summoner data, includes IMPORTANT matchHistoryURL
 
+Timeline is broken into frames, each of which encompasses a minute of the game.
+
+Each frame contains:    timestamp (beginning of frame), 
+                        participantFrames (dict) snapshot of all 10 players
+                        events (dict) list of events occurring within the frame 
+
+
+Event Handling types:
+each EVENT has an optional: assistingParticipantIds field for other participants (list type)
+
+Skill:  SKILL_LEVEL_UP:  level up skill event, by participant + skillslot
+            LEVEL_UP_TYPE: EVOLVE for viktor/khazix/??syndra??, NORMAL for everything else
+
+Item:   ITEM_PURCHASE - buy
+        ITEM_SOLD - sell
+        ITEM_UNDO - undo buy or sell (item_before indicates what item was, item_after...)
+        ITEM_DESTROYED - item used (potions)
+
+Ward:   WARD_PLACED - timestamp, type, creatorID
+        WARD_KILL - killerID, timetamp, type
+
+Champ kill:   
+        assistingParticipantIds (list)
+        killerId
+        position (x,y on the map)
+
+BUILDING_KILL:
+        assistingParticipantIds
+        buildingType - tower, inhib
+        killerID
+        laneType - BOT, MID, TOP
+        towerType - OUTER, INNER, BASE, NEXUS, UNDEFINED (azir)
+        
 
 
 
 '''
+
+
+
 
 
 if __name__ == '__main__':
