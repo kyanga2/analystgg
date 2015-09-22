699ad7f694dd5b36a8df8552d0a6c1781b7f7476
abzgupta <gSchool@Datas-Mac-mini-25.local>
2015-09-21 17:51:03 -0700

    invades in progress

--- analyser.py
+++ analyser.py
@@ -88,7 +88,7 @@ _role_dict = {'TOPSOLO' : 'TOP',
                 'MIDDUO': 'TROLL',
                 'TOPDUO': 'TROLL'}
 
-def pullStats(match): 
+def pull_stats(match): 
     stats_out = [{'id':x} for x in xrange(1,11)]
 
     #initialize counter variables for calculating stats
@@ -123,7 +123,8 @@ def pullStats(match):
     lane_advantage = [0]*10
     jg_invade_red = False
     jg_invade_blue = False
-    invade_coefficient = [0]*10
+    invade_offense = [0]*10
+    invade_defense = [0]*10
 
     '''
     Initialize all 10 participant roles
@@ -135,8 +136,71 @@ def pullStats(match):
 
 
     #iterate through all frames within the timeline, one minute at a time.
-    #no important data in first frame
-    for i in xrange(1,len(match['timeline']['frames'])):
+    #no dynamic data in 0 frame, occasionally no events occur during 2 frame
+
+    '''
+    Check for invades which occur during frames 1 and 2
+    100 = blue team, 200 = red team
+    x+y < 13500 = blue side
+    x+y > 16500 = red side
+    inbetween = river
+
+    '''
+    try:
+        for i in xrange(2):
+
+            for event in match['timeline']['frames'][i]['events']:
+                if event['eventType'] == 'CHAMPION_KILL':
+                    v_id = event['victimId']-1
+
+                    x_coord = event['position']['x']
+                    y_coord - event['position']['y']
+                    if x_coord + y_coord  < 14500:
+                        #blue side invaded
+                        if v_id <= 5:
+                            invade_defense[v_id] -= 1
+                            invade_offense[event['killerId']-1] += 1
+                            try:
+                                for champ in event['assistingParticipantIds']:
+                                    invade_offense[champ] += 1
+                            except KeyError:
+                                pass
+                        else:
+                            invade_offense[v_id] -= 1
+                            invade_defense[event['killerId']-1] += 1
+                            try:
+                                for champ in event['assistingParticipantIds']:
+                                    invade_defense[champ] += 1
+                            except KeyError:
+                                pass
+                    else:
+                        #red side invaded, river brawls credited to red side
+                        if v_id <= 5:
+                            invade_offense[v_id] -= 1
+                            invade_defense[event['killerId']-1] += 1
+                            try:
+                                for champ in event['assistingParticipantIds']:
+                                    invade_defense[champ] += 1
+                            except KeyError:
+                                pass
+                        else:
+                            invade_defense[v_id] -= 1
+                            invade_offense[event['killerId']-1] += 1
+                            try:
+                                for champ in event['assistingParticipantIds']:
+                                    invade_offense[champ] += 1
+                            except KeyError:
+                                pass
+    except:
+        pass
+
+
+
+
+
+
+
+    for i in xrange(3,len(match['timeline']['frames'])):
 
         #iterate through events for minute ending in i
 
@@ -152,6 +216,12 @@ def pullStats(match):
             if i<10: ftmz = '0'
             stats_out[pNum]['csmin'+ftmz+str(i)] =cs_at - cs_min_counter[pNum]
             cs_min_counter[pNum] = cs_at            
+
+
+    stats_out[i]['invade_offense'] = invade_offense[i]
+    stats_out[i]['invade_defense'] = invade_defense[i]
+
+
 
     return stats_out
 
@@ -172,7 +242,9 @@ def survey_stats(match, event_types):
             print KeyError, "frame", i, "from match", match['matchId']
 
 
-
+def run_time(matches):
+    for i in xrange(len(matches)):
+        pull_stats(matches[i])
 
 
 if __name__ == '__main__':
@@ -181,15 +253,15 @@ if __name__ == '__main__':
     match_list = apic.get_match_list('na', summ_list['vaiorswift'], '')
     counter = 0
     event_types = {}
-    time.sleep(5)
-    for i in xrange(335, 336):
+    time.sleep(11)
+    match = [None]*10
+    for i in xrange(10):
         try:
-            match = apic.get_match('na',match_list.keys()[i], True) #catches 404s
-            if match:
-                survey_stats(apic.get_match('na',match_list.keys()[i], True), event_types)
+            match[i] = apic.get_match('na',match_list.keys()[i], True) #catches 404s
+            if match[i]:
+                survey_stats(match[i], event_types)
                 counter += 1
                 print i
-                time.sleep(1)
             else:
                 print "Failed to retrieve match 404, match ID:", match_list.keys()[i]
             if counter == 10:
